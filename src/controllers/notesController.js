f5026ae60181d315f7317fb2af024de3282d9ec3
bndnt <67003594+bndnt@users.noreply.github.com>
2026-01-27 21:33:25 -0500

    Add pagination, search and request validation

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -2,8 +2,24 @@ import createHttpError from 'http-errors';
 import { Note } from '../models/note.js';
 
 export const getAllNotes = async (req, res) => {
-  const notes = await Note.find();
-  res.status(200).json(notes);
+  const { page = 1, perPage = 15, tag, search } = req.query;
+  const skip = (page - 1) * perPage;
+  // const notes = await Note.find();
+  const notesQuery = Note.find();
+  if (tag) {
+    notesQuery.where('tag').equals(tag);
+  }
+  if (search) {
+    notesQuery.where({ $text: { $search: search } });
+  }
+  const [totalNotes, notes] = await Promise.all([
+    notesQuery.clone().countDocuments(),
+    notesQuery.skip(skip).limit(perPage),
+  ]);
+
+  const totalPages = Math.ceil(totalNotes / perPage);
+
+  res.status(200).json({ page, perPage, totalPages, tag, search, notes });
 };
 
 export const getNoteById = async (req, res) => {
--- src/models/note.js
+++ src/models/note.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { TAGS } from '../constants/tags.js';
 
 const noteSchema = mongoose.Schema(
   {
@@ -14,21 +15,13 @@ const noteSchema = mongoose.Schema(
     },
     tag: {
       type: String,
-      enum: [
-        'Work',
-        'Personal',
-        'Meeting',
-        'Shopping',
-        'Ideas',
-        'Travel',
-        'Finance',
-        'Health',
-        'Important',
-        'Todo',
-      ],
+      enum: TAGS,
       default: 'Todo',
     },
   },
   { timestamps: true },
 );
+
+noteSchema.index({ title: 'text', content: 'text' });
+
 export const Note = mongoose.model('Note', noteSchema);
