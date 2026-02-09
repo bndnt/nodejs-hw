770a00bb6fe610e692b9d0da1b7f4eb0a156ac05
bndnt <67003594+bndnt@users.noreply.github.com>
2026-02-09 17:11:04 -0500

    bot debug 2

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -80,7 +80,9 @@ export const requestResetEmail = async (req, res, next) => {
   const { email } = req.body;
   const user = await User.findOne({ email });
   if (!user) {
-    res.status(200).json({ message: 'Password reset email sent successfully' });
+    return res
+      .status(200)
+      .json({ message: 'Password reset email sent successfully' });
   }
   const resetToken = jwt.sign(
     { sub: user._id, email },
--- src/utils/saveFileToCloudinary.js
+++ src/utils/saveFileToCloudinary.js
@@ -7,7 +7,7 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
-export const saveFileToCloudinary = async (buffer, userId) => {
+export const saveFileToCloudinary = async (buffer) => {
   return new Promise((resolve, reject) => {
     const uploadStream = cloudinary.uploader.upload_stream(
       {
